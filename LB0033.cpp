75f4b77805babccca1f6124825d9c74b30a0646a
vishal230120 <113354369+vishal230120@users.noreply.github.com>
2022-10-14 17:22:36 +0530

    fgjfcvkvjf ncfn

---
+++ LB0033.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a,b;
+  for(a=1;a<=5;a++)
+  {
+       for(b=1;b<=2*a-1;b++)
+       {
+              printf("%d",b);
+        }
+              printf("\n");
+      } 
+      return 0;   
+}
